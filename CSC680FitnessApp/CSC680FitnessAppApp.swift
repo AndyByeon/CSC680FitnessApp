4ca8e07df01bd6c9e1f188a7657b321a2db8de12
AndyByeon <abyeon@mail.sfsu.edu>
2024-03-28 15:04:48 -0700

    Initial Commit

---
+++ CSC680FitnessApp/CSC680FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSC680FitnessAppApp.swift
+//  CSC680FitnessApp
+//
+//  Created by Andy Byeon on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct CSC680FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
