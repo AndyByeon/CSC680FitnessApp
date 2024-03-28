24345212247993857357b0bc0a8b1ec6be4b9933
AndyByeon <abyeon@mail.sfsu.edu>
2024-03-28 16:24:00 -0700

    created a picker so the user can set the amount for the timer.

--- CSC680FitnessApp/ContentView.swift
+++ CSC680FitnessApp/ContentView.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    let secondsOption = 0...60
+    
+    @State
+    var defaultSecond = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello!")
+            Text("Welcome to the timer section of the app.").foregroundColor(.blue)
+            Text("Select the amount of seconds for the timer.").foregroundColor(.blue)
+            
+            HStack{
+                Picker("", selection: $defaultSecond) {
+                    ForEach(secondsOption, id: \.self) { (selectedseconds: Int) in Text(String(selectedseconds))
+                        
+                    }
+                }
+            }
         }
         .padding()
     }
