64b4b7f6b552784b78a19a084314898e484147b4
AndyByeon <abyeon@mail.sfsu.edu>
2024-03-28 18:51:56 -0700

    Added a start stop and reset to the timer which makes the timer count down

--- CSC680FitnessApp/ContentView.swift
+++ CSC680FitnessApp/ContentView.swift
@@ -8,22 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     let secondsOption = 0...60
     
     @State
     var defaultSecond = 0
     
+    @State
+    var timerRunning = false
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        VStack {
-            Text("Welcome to the timer section of the app.").foregroundColor(.blue)
-            Text("Select the amount of seconds for the timer.").foregroundColor(.blue)
-            
-            HStack{
+        VStack(spacing: 30){
+            Text("Timer").font(.system(size: 70))
                 Picker("", selection: $defaultSecond) {
                     ForEach(secondsOption, id: \.self) { (selectedseconds: Int) in Text(String(selectedseconds))
-                        
                     }
                 }
+            
+            Text("\(defaultSecond) sec").font(.system(size: 70)).onReceive(timer) { _ in
+                if defaultSecond > 0 && timerRunning {
+                    defaultSecond -= 1
+                } else {
+                    timerRunning = false
+                }
+            }
+            
+            HStack(spacing: 30){
+                Button("Start") {
+                    timerRunning = true
+                }.foregroundColor(.green)
+                
+                Button("Stop") {
+                    timerRunning = false
+                }.foregroundColor(.red)
+                
+                Button("Reset") {
+                    defaultSecond = 0
+                }.foregroundColor(.red)
             }
         }
         .padding()
